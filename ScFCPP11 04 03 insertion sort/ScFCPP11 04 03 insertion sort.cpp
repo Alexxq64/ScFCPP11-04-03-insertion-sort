efddbfdbe9a9dba375bf019aad9f66b3c69fc267
Alex Kuznetsov <alexxq@list.ru>
2023-04-10 14:51:34 +0300

    Добавьте файлы проекта.

---
+++ ScFCPP11 04 03 insertion sort/ScFCPP11 04 03 insertion sort.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+
+
+void insertionSort(int* arr, int n)
+{
+	for (int i = 1; i < n; i++) {
+		int key = arr[i];
+		for (int k = i - 1; k >= 0 && key < arr[k]; k--) {
+			arr[k + 1] = arr[k];
+			arr[k] = key;
+		}
+	}
+}
+
+
+int main()
+{
+	int arr[] = { 38, 27, 43, 3, 9, 82, 10 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+	for (int i = 0; i < n; i++)
+		std::cout << arr[i] << " ";
+	std::cout << std::endl;
+	insertionSort(arr, n);
+	std::cout << std::endl;
+	for (int i = 0; i < n; i++)
+		std::cout << arr[i] << " ";
+}
+
